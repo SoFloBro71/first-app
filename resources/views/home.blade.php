57a7bfe037679329119540f836a64a7cdddbea54
Hannah Farrelly Spain (Student) <N00211013@iadt.ie>
2022-09-30 11:58:53 +0100

    started LinkedIn

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.based')
+
+@section('content')
+
+<h1 class="text-4xl font-bold">Home</h1>
+
+<p class="mt-4 text-lg"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet iste aliquid omnis a praesentium molestias eius consequuntur dolorem quas. Iure numquam facilis ullam voluptatibus aut illum error harum quidem eos!
+</p>
+
+@endsection
